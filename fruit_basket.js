cfd7b39f38bd3d6251fa8bd525f3de4a548fac6b
soumyasrishti01 <78192188+soumyasrishti01@users.noreply.github.com>
2021-11-27 17:14:24 +0530

    Add files via upload

---
+++ fruit_basket.js
@@ -0,0 +1,48 @@
+status = "";
+img = "";
+objectDetection = "";
+objects = [];
+
+function preload() {
+    img = "AC pic";
+}
+
+function setup() {
+    canvas = createCanvas(600, 400);
+    canvas.center();
+
+    objectDetection = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("fruit_detect").innerHTML = "Detecting Object";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw() {
+    image(img, 0, 0, 600, 400);
+
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("fruit_detect").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", object[i].x + 15, obejcts[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
